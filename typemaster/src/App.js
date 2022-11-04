2cb3fe9267a77c524772d043a21689b909d20e24
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-11-04 12:41:18 +0530

    typemaster footer

--- typemaster/src/App.js
+++ typemaster/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 import Navbar from './Components/Navbar';
 import AllRoutes from './Components/AllRoutes';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
    
       <Navbar />
-      <AllRoutes/>
+      <AllRoutes />
+      <Footer/>
     </div>
   );
 }
--- typemaster/src/Pages/Home.jsx
+++ typemaster/src/Pages/Home.jsx
@@ -3,7 +3,8 @@ function Home() {
 
     return (
         <div>
-<h1>Home page</h1>
+            <h1>Home page</h1>
+            
         </div>
     )
 }
