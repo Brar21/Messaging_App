d277929eed6f9aa681eb07cedde6188645e70e07
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-11-20 11:59:08 +0530

    daily work

--- typemaster/src/Pages/Test.jsx
+++ typemaster/src/Pages/Test.jsx
@@ -1,44 +1,25 @@
 import React from 'react'
-import {
-    AlertDialog,
-    AlertDialogBody,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogContent,
-    AlertDialogOverlay,
-    useDisclosure,
-} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import { Text,Button } from '@chakra-ui/react';
+import { Text, Button, Box } from '@chakra-ui/react';
+
 export const TestSpeed = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+
 
   return (
-      <div >
+      <Box w='100%' bg='#577b87'>
+          <Box w='70%' display='flex' m='auto' gap={12} p='5rem 0'>
+          <Box bg='white' borderRadius={10} w='80%' >
+              <Text>Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.</Text>
+          </Box>
+          <Box display='flex' flexDirection='column'>
+              <Text>SPEED</Text>
+              <span>0 WPM</span>
+              <Text>ACCURACY</Text>
+                  <span>100%</span>
+                  <Button>Restart</Button>
+              </Box>
+          </Box>
 
-          <AlertDialog
-              isOpen={isOpen}
-              onClose={onClose}
-          >
-              <AlertDialogOverlay>
-                  <AlertDialogContent w="90%">
-                      <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-
-                      </AlertDialogHeader>
-
-                      <AlertDialogBody>
-                          You can <Link to='/donate'><Text _hover={{ color: "blue", textDecoration: 'undeline' }}>thanks us and Donate</Text></Link>
-                      </AlertDialogBody>
-
-                      <AlertDialogFooter>
-                          <Button onClick={onClose}>
-                              Start Typing Test
-                          </Button>
-
-                      </AlertDialogFooter>
-                  </AlertDialogContent>
-              </AlertDialogOverlay>
-          </AlertDialog>
-    </div>
+     </Box>
   )
 }
