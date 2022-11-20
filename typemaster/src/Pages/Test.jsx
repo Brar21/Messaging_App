8c50a83e6b76f7c9133bce19786258323af3bbf7
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-11-20 14:05:28 +0530

    daily work

--- typemaster/src/Pages/Test.jsx
+++ typemaster/src/Pages/Test.jsx
@@ -1,25 +1,61 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import { Text, Button, Box } from '@chakra-ui/react';
-
+import { Link } from '@chakra-ui/react';
+import { Text, Button, Box, useDisclosure } from '@chakra-ui/react';
+import { SlSpeedometer } from "react-icons/sl";
+import { GiArcheryTarget } from "react-icons/gi";
+import { VscDebugRestart } from 'react-icons/vsc';
+import { VscDebugStart, } from 'react-icons/vsc';
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+} from '@chakra-ui/react';
 export const TestSpeed = () => {
 
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <Box w='100%' bg='#577b87'>
+            <Box w='70%' display="flex" flexdirection="row" m="auto" h='auto'  p='5rem 0' gap={12}>
+                <Box bg="white" h='45vh' borderRadius={10} w='80%' m='auto'>
+                    <Text textAlign='start' p='1.5rem' fontSize={25} color='gray.500'>Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.</Text>
+                </Box>
+                <Box  >
+                    <Text fontSize={25} fontWeight='700' color='whatsapp.500' display="flex" gap={3}><Text pt='5px' fontSize={28} color='white'><SlSpeedometer /></Text>SPEED</Text>
+                    <span><Text fontFamily='mono' fontWeight='800' fontSize={28} m='8px' color='facebook.200'>0 WPM</Text></span>
+                    <Text fontSize={25} fontWeight='700' color='whatsapp.500' display="flex" gap={3}><Text pt='5px' fontSize={28} color='white'><GiArcheryTarget /></Text>ACCURACY</Text>
+                    <span><Text fontFamily='mono' fontWeight='800' fontSize={28} m='8px'  color='facebook.200'>100%</Text></span>
+                    <Button mt='2rem' bg='none' color='#ffe318' fontSize={25} _hover={{ bg: 'none' }} onClick={onOpen}><Text pr='5px'><VscDebugRestart /></Text>Restart</Button>
+                    <AlertDialog
+                        isOpen={isOpen}
+                        onClose={onClose}
+                        isCentered
+                        size="2xl"
 
-  return (
-      <Box w='100%' bg='#577b87'>
-          <Box w='70%' display='flex' m='auto' gap={12} p='5rem 0'>
-          <Box bg='white' borderRadius={10} w='80%' >
-              <Text>Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.</Text>
-          </Box>
-          <Box display='flex' flexDirection='column'>
-              <Text>SPEED</Text>
-              <span>0 WPM</span>
-              <Text>ACCURACY</Text>
-                  <span>100%</span>
-                  <Button>Restart</Button>
-              </Box>
-          </Box>
+                    >                                <AlertDialogOverlay  >
+                            <AlertDialogContent  >
+                                <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                                    <Text bg='beige' w='150px' h='150px' borderRadius='50%' m='auto' p="5% 5.8%"><VscDebugStart fontSize={90} fill='green' /></Text>
+                                </AlertDialogHeader>
 
-     </Box>
-  )
+                                <AlertDialogBody m='auto' fontSize={23} fontWeight='600'>
+                                    <Text display='flex'> Prepare yourself before start typing...</Text>
+                                </AlertDialogBody>
+
+                                <AlertDialogFooter m='1rem auto'>
+                                    <Button onClick={onClose} colorScheme='whatsapp' p='2rem 2.5rem' fontSize={30}>
+                                        <Link herf="/testspeed/test">Start Typing Again</Link>
+                                    </Button>
+
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialogOverlay>
+                    </AlertDialog>
+                </Box>
+            </Box>
+
+        </Box >
+    )
 }
