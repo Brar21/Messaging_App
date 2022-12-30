9e7cad008a6d674c76d14a027e08c83c65073723
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-30 16:33:11 +0530

    useRef

--- Typescript/useRef.hook/src/App.jsx
+++ Typescript/useRef.hook/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState("")
-
+    const [name,setName]=useState("");
+    const [renderCount,setRenderCount]=useState(0)
+    useEffect(() =>
+    {
+    setRenderCount(prev=>prev+1)
+})
   return (
     <div className="App">
           <input value={name} onChange={e => setName(e.target.value)} />
           <div>My Name is {name}</div>
-     
+          <div>I rendered {renderCount} times</div>
     </div>
   )
 }
