78903e930c363db1c41fc1db4cc1dd1e7f8a1ce5
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-12-30 17:03:28 +0530

    useRef

--- Typescript/useRef.hook/src/App.jsx
+++ Typescript/useRef.hook/src/App.jsx
@@ -1,19 +1,30 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import {useRef} from 'react';
 
 function App() {
     const [name,setName]=useState("");
-    const [renderCount,setRenderCount]=useState(0)
-    useEffect(() =>
+    //const [renderCount,setRenderCount]=useState(0)//bad practice
+    //const renderCount=useRef(1)
+//    useEffect(() =>
+//    {
+//    //setRenderCount(prev=>prev+1)
+//        renderCount.current=renderCount.current+1
+//}) //simple way
+    const inputRef=useRef();
+    function focus()
     {
-    setRenderCount(prev=>prev+1)
-})
+        inputRef.current.focus()
+        inputRef.current.value="useRef Hook";
+        
+    }
   return (
     <div className="App">
-          <input value={name} onChange={e => setName(e.target.value)} />
+          <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
           <div>My Name is {name}</div>
-          <div>I rendered {renderCount} times</div>
+          {/*<div>I rendered {renderCount.current} times</div>*/}
+          <button onClick={focus}>Focus</button>
     </div>
   )
 }
