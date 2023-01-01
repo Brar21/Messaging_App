831a24be613bda2bb09e6e8f8c69c31a4d0ab92f
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-01 17:40:07 +0530

    useMemo.hook

--- Typescript/useMemo.hook/src/App.jsx
+++ Typescript/useMemo.hook/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
     const [num,setNum]=useState(0)
     const [darkWeb,SetDarkweb]=useState(false)
-
+const Multiply2Number=ReduceSpeed(num)
     const themeChanger={
         backgroundColor: darkWeb? "yellow":"gray",
         color: darkWeb? "gray":"yellow"
@@ -14,8 +14,22 @@ function App() {
     <div className="App" style={themeChanger}>
           <input type="text" value={num} onChange={() => setNum(parseInt(e => e.target.value))} />
           <button onClick={() => SetDarkweb(dark!=dark)}>Theme Toggler</button>
+          <div></div>
     </div>
   )
 }
+
+function ReduceSpeed(number)
+{
+    console.log("how much slow?")
+    for(let i=0;i<=10000000000;i++)
+    {
+        return num*2
+    }
+}
+
+
+
+
 
 export default App
