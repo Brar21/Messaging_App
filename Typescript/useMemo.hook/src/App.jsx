73231fe88117f421d1daa13c1167f170cdc0ec23
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-01 17:36:39 +0530

    useMemo.hook

--- Typescript/useMemo.hook/src/App.jsx
+++ Typescript/useMemo.hook/src/App.jsx
@@ -1,32 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [num,setNum]=useState(0)
+    const [darkWeb,SetDarkweb]=useState(false)
+
+    const themeChanger={
+        backgroundColor: darkWeb? "yellow":"gray",
+        color: darkWeb? "gray":"yellow"
+    }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App" style={themeChanger}>
+          <input type="text" value={num} onChange={() => setNum(parseInt(e => e.target.value))} />
+          <button onClick={() => SetDarkweb(dark!=dark)}>Theme Toggler</button>
     </div>
   )
 }
