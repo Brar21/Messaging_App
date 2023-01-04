fffd0c74ac8538fd3008809cebcf51dca3501f3c
Brar21 <103635395+Brar21@users.noreply.github.com>
2023-01-04 16:15:50 +0530

    useContext.hook

--- Typescript/useContext.hook/src/App.jsx
+++ Typescript/useContext.hook/src/App.jsx
@@ -1,18 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState,createContext,useContext } from 'react'
 import './App.css'
-
+const Count=createContext()
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+    return (
+      <Count.Provider value={count}>
       <div className="App">
           <h1>Counter : {count}</h1>
-          <button>+</button>
-          <button>-</button>
-          
-    </div>
+          <br />
+          <br />
+          <button onClick={()=>setCount(count+1)}>+</button>
+          <button onClick={()=>setCount(count-1)}>-</button>
+          <Counter2/>
+            </div>
+            </Count.Provider>
   )
 }
-
+function Counter2()
+{
+    return (
+        <>
+        <h1>Counter 2</h1>
+        <Counter3/>
+        </>
+    )
+}
+function Counter3()
+{
+    return (
+        <>
+        <h1>Counter 3</h1>
+        <Counter4/>
+        </>
+    )
+}
+function Counter4()
+{
+    return (
+        <>
+        <h1>Counter 4</h1>
+        <Counter5/>
+        </>
+    )
+}
+function Counter5()
+{
+    const counter=useContext(Count)
+    return (
+        <h1>Counter 5</h1>
+    )
+}
 export default App
